7616d6d06ce5b77567a2ad45be2bb21ff393d30c
Dorian <dorianmare@yahoo.fr>
2019-12-03 14:22:13 +0100

    protection url

--- include/texte.inc.php
+++ include/texte.inc.php
@@ -1,104 +1,136 @@
 <div id="texte">
 <?php
+
 if (!empty($_GET["page"])) {
-    $page = $_GET["page"];} else { $page = 0;
+  $page = $_GET["page"];
 }
+else {
+  $page = 0;
+}
+
 switch ($page) {
-//
+
+    //
     // Personnes
     //
 
     case 0:
-        // inclure ici la page accueil photo
-        include_once 'pages/accueil.inc.php';
-        break;
+      // inclure ici la page accueil photo
+      include_once 'pages/accueil.inc.php';
+      break;
     case 1:
-        // inclure ici la page liste des personnes
-        include "pages/listerPersonnes.inc.php";
-        break;
-
+      // inclure ici la page liste des personnes
+      include "pages/listerPersonnes.inc.php";
+      break;
     case 2:
+      if ($_SESSION['connexion']) {
         // inclure ici la page ajouter personnes
         include_once 'pages/ajouterPersonne.inc.php';
-        break;
+      }
+      break;
+    //premiere page de modifier personne
     case 3:
+      if ($_SESSION['estAdmin']) {
         // inclure ici la page modification des personnes
         include "pages/modifierPersonnes.inc.php";
-        break;
+      }
+      break;
     case 4:
+      if ($_SESSION['estAdmin']) {
         // inclure ici la page suppression personnes
         include_once 'pages/supprimerPersonne.inc.php';
-        break;
-//
-    // Citations
-    //
-
-    case 5:
-        // inclure ici la page ajouter citations
-        include "pages/ajouterCitation.inc.php";
-        break;
-    case 6:
-        // inclure ici la page liste des citations
-        include "pages/listerCitation.inc.php";
-        break;
-
-    case 7:
-        // inclure ici la page rechercher citations
-            include "pages/rechercherCitation.inc.php";
-        break;
-    case 8:
-        // inclure ici la page valider citations
-            include "pages/validerCitation.inc.php";
-        break;
-    case 9:
-        // inclure ici la page supprimer citations
-           include "pages/supprimerCitation.inc.php";
-        break;
-
-//
-    // Villes
-    //
-
-    case 10:
-// inclure ici la page lister ville
-        include "pages/listerVilles.inc.php";
-        break;
-
-    case 11:
-        // inclure ici la page ajouter ville
-        include "pages/ajouterVille.inc.php";
-        break;
-
-    case 12:
-        // inclure ici la page modifier ville
-        include "pages/modifierVille.inc.php";
-        break;
-    case 13:
-        // inclure ici la page supprimer ville
-        include "pages/supprimerVille.inc.php";
-        break;
-    case 14:
-        // inclure ici la page supprimer ville
-        include "pages/connexion.inc.php";
-        break;
-    case 15:
-        // inclure ici la page supprimer ville
-        include "pages/detailPersonne.inc.php";
-        break;
+      }
+      break;
+    //seconde page de modifier personne
     case 16:
       //inclure ici la page permettant de modifier une personnes
       include "pages/modifierPersonne.inc.php";
       break;
+
+    //
+    // Citations
+    //
+
+    case 5:
+      if ($_SESSION['estEtudiant']) {
+        // inclure ici la page ajouter citations
+        include "pages/ajouterCitation.inc.php";
+      }
+      break;
+    case 6:
+      // inclure ici la page liste des citations
+      include "pages/listerCitation.inc.php";
+      break;
+    case 7:
+      if ($_SESSION['connexion']) {
+        // inclure ici la page rechercher citations
+        include "pages/rechercherCitation.inc.php";
+      }
+        break;
+    case 8:
+      if ($_SESSION['estAdmin']) {
+        // inclure ici la page valider citations
+        include "pages/validerCitation.inc.php";
+      }
+      break;
+    case 9:
+      if ($_SESSION['estAdmin']) {
+        // inclure ici la page supprimer citations
+        include "pages/supprimerCitation.inc.php";
+      }
+      break;
     case 17:
-      //inclure ici la page permettant de modifier une personnes
-      include "pages/noterCitation.inc.php";
+      if ($_SESSION['estEtudiant']) {
+        //inclure ici la page permettant de noter une citation
+        include "pages/noterCitation.inc.php";
+      }
+      break;
+
+    //
+    // Villes
+    //
+
+    case 10:
+      // inclure ici la page lister ville
+      include "pages/listerVilles.inc.php";
+      break;
+    case 11:
+      if ($_SESSION['connexion']) {
+        // inclure ici la page ajouter ville
+        include "pages/ajouterVille.inc.php";
+        break;
+      }
+    case 12:
+      if ($_SESSION['connexion']) {
+        // inclure ici la page modifier ville
+        include "pages/modifierVille.inc.php";
+        break;
+      }
+    case 13:
+      if ($_SESSION['estAdmin']) {
+        // inclure ici la page supprimer ville
+        include "pages/supprimerVille.inc.php";
+      }
+      break;
+
+    //
+    //Connexion
+    //
+
+    case 14:
+      include "pages/connexion.inc.php";
+      break;
+    case 15:
+      //page montrant les details d'une personne
+      include "pages/detailPersonne.inc.php";
       break;
     case 18:
       //inclure ici la page qui permet de se deconnecter
       include "pages/deconnexion.inc.php";
       break;
 
-    default:include_once 'pages/accueil.inc.php';
+    default:
+      include_once 'pages/accueil.inc.php';
 }
 
 ?>
